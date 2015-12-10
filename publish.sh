b28b45025dbaa1e33ee315f30c1b7d8ef35f1779
tsamis73 <tsamis73@gmail.com>
2015-12-10 13:28:50 +0100

    Removed comment lines

--- publish.sh
+++ publish.sh
@@ -3,7 +3,5 @@ cd output
 git init
 git remote add output https://github.com/tsamis73/tsamis73.github.io.git
 git add *
-#echo -n "Give the commit message:   "
-#read commit
-git commit -a #-m "$commit"
+git commit -a 
 git push output master -f
